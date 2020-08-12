3fb6917449bd01c979845a262aae85297e53f424
tiagobani <tiago.bani@yahoo.com.br>
2020-08-11 22:51:07 -0300

    [feat]: add inputs select and picker

--- mobile/src/components/Select/index.tsx
+++ mobile/src/components/Select/index.tsx
@@ -1,1 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Picker, Text, View } from 'react-native';
+
+import styles from './styles'
+
+interface SelectProps {
+    label: string,
+    items: { option: string, value: string }[],
+    setValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Select: React.FC<SelectProps> = ({ label, setValue, items }) => {
+    const [selectedValue, setSelectedValue] = useState("java");
+    return (<>
+        <Text style={styles.label}>{label}</Text>
+        <View style={styles.input}>
+            <Picker
+                selectedValue={selectedValue}
+                onValueChange={setSelectedValue}
+                style={{ width: 150, height: 150 }}
+            >
+                {items?.map(({ option, value }) => {
+                    return (
+                        <Picker.Item key={value} label={option} value={value} />
+                    )
+                })}
+            </Picker>
+        </View>
+    </>
+    );
+}
+
+export default Select
